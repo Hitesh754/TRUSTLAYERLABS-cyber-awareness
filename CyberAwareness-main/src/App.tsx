3c81537f1f5e010bd8f430a591f2d54020be3223
Adil Sukumar <adilsukumar24@gmail.com>
2026-05-20 21:43:45 +0530

    fix: scroll to top on every page navigation

--- CyberAwareness-main/src/App.tsx
+++ CyberAwareness-main/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import ThreatBanner from './components/ThreatBanner';
@@ -31,6 +32,7 @@ function HomePage() {
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/"                          element={<HomePage />} />
         <Route path="/awareness"                 element={<AwarenessIndex />} />
